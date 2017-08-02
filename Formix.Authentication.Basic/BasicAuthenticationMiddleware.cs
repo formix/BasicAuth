05e74457c571f25089bfd8bc17f435b944ace386
formix <jpg@formix.org>
2017-08-01 21:52:19 -0400

    Sorted and cleared usings

--- Formix.Authentication.Basic/BasicAuthenticationMiddleware.cs
+++ Formix.Authentication.Basic/BasicAuthenticationMiddleware.cs
@@ -1,9 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using System;
-using System.Collections.Generic;
 using System.Security.Claims;
 using System.Text;
-using System.Threading;
 using System.Threading.Tasks;
 
 namespace Formix.Authentication.Basic
